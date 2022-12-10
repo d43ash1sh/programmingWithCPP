fa9175a5d68ad9afde1ac1b0317d260b3b649a99
DEBASHISH BORDOLOI <webshack2018@icloud.com>
2022-12-10 21:20:51 +0530

    organizing-the-code

---
+++ day01/OmittingNamespace.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!";
+  return 0;
+}
+// You might see some C++ programs that runs without the standard namespace library. 
+// The using namespace std line can be omitted and replaced with the std keyword, 
+// followed by the :: operator for some objects
